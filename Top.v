e73584b7b35e8cfa7d0f2a21f1fd7f60285d7bd9
Dr. ERDI Gergo <gergo@erdi.hu>
2021-10-16 21:01:05 +0800

    Simple, but non-combinational, seven-segment-driving circuit

--- Top.v
+++ Top.v
@@ -1,4 +1,5 @@
 module Top(
+           input        CLK100MHZ,
            input [7:0]  SW,
            output [7:0] AN,
            output [6:0] SEG,
@@ -8,7 +9,7 @@ module Top(
    assign AN[7:4] = 4'b1111;
    
    topEntity u_topEntity
-     (
+     (.CLK(CLK100MHZ),
       .SWITCHES(SW[7:0]),
       .SS_AN(AN[3:0]),
       .SS_SEG(SEG[6:0]),
